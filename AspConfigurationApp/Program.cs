ad6d61094e312418abd4dde10fa0089f72759f22
Maxim Efimov <efimov.top.academy@gmail.com>
2024-12-03 19:24:41 +0300

    03-12-24

--- AspConfigurationApp/Program.cs
+++ AspConfigurationApp/Program.cs
@@ -1,11 +1,30 @@
+using AspConfigurationApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddInMemoryCollection(
-    new Dictionary<string, string>
-    {
-        { "login", "admin" },
-        { "password", "qwerty" }
-    });
+//builder.Configuration
+//       .AddInMemoryCollection(
+//        new Dictionary<string, string>
+//        {
+//            { "login", "admin" },
+//            { "password", "qwerty" }
+//        });
+
+//builder.Configuration
+//       .AddJsonFile("config.json")
+//       .AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile("config.json");
+UserConfig userConfig = new();
+builder.Configuration
+       .Bind(userConfig);
+
+//builder.Configuration
+//       //.AddXmlFile("config.xml")
+//       .AddXmlFile("appsettings.xml");
+
+//builder.Configuration
+//       .AddIniFile("config.ini");
+
 
 var app = builder.Build();
 
@@ -22,7 +41,14 @@ var app = builder.Build();
 //    await context.Response.WriteAsync(responseString);
 //});
 
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["login"]}");
 
+
+
+//app.Map("/", (IConfiguration appConfig) => $"{appConfig["login"]} - {appConfig["password"]}");
+//app.Map("/", (IConfiguration appConfig) => $"{appConfig["Employee:Name:First"]}");
+
+app.Run(async context => await context.Response
+                                      .WriteAsync(userConfig.ToString()));
+                                      //.WriteAsJsonAsync(userConfig));
 
 app.Run();
---
+++ AspConfigurationApp/UserConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace AspConfigurationApp
+{
+    public class UserConfig
+    {
+        public string Login { get; set; } = "";
+        public string Password { get; set; } = "";
+        public override string ToString()
+        {
+            return $"Config info. Login: {Login}, Password: {Password}";
+        }
+    }
+}
